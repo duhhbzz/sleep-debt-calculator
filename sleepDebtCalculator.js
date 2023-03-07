82f7c5470b6f7c221cec03f9d930121ce05e7697
duhhbzz <77603188+duhhbzz@users.noreply.github.com>
2023-03-07 00:01:17 -0600

    edits
    all the edits

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -1,50 +1,51 @@
-const getSleepHours = day =>  {
-  switch (day)  {
-    case 'sunday':
-      return 8;
-      break;
-    case 'monday':
-      return 7;
-      break;
-    case 'tuesday':
-      return 7;
-      break;
-    case 'wednesday':
-      return 8;
-      break;
-    case 'thursday':
-      return 7;
-      break;
-    case 'friday':
-      return 4;
-      break;
-    case 'saturday':
-      return 4;
-      break;
-  };
-};
-
-const getActualSleepHours = () =>
-  getSleepHours('sunday') +
-  getSleepHours('monday') +
-  getSleepHours('tuesday') +
-  getSleepHours('wednesday') +
-  getSleepHours('thursday') +
-  getSleepHours('friday') +
-  getSleepHours('saturday');
-
-const getIdealSleepHours = () => {
-  const idealHours = 8;
-  return idealHours * 7;
-};
-
-const actualSleepHours = getActualSleepHours()
-const idealSleepHours = getIdealSleepHours()
-
-if (actualSleepHours === idealSleepHours) {
-  console.log('Perfect Sleep Amount Achieved')
-} else if (actualSleepHours > idealSleepHours)  {
-  console.log('You\'ve slept more than nessecsary')
-} else if (actualSleepHours < idealSleepHours)  {
-  console.log('Go to bed, you need more rest')
+function getSleepHours(day) {
+    switch (day) {
+        case 'sunday':
+            return 8;
+            break;
+        case 'monday':
+            return 7;
+            break;
+        case 'tuesday':
+            return 7;
+            break;
+        case 'wednesday':
+            return 8;
+            break;
+        case 'thursday':
+            return 7;
+            break;
+        case 'friday':
+            return 4;
+            break;
+        case 'saturday':
+            return 4;
+            break;
+    };
 }
+  
+  function getActualSleepHours() {
+    return getSleepHours('sunday') +
+        getSleepHours('monday') +
+        getSleepHours('tuesday') +
+        getSleepHours('wednesday') +
+        getSleepHours('thursday') +
+        getSleepHours('friday') +
+        getSleepHours('saturday');
+}
+  
+  function getIdealSleepHours() {
+    const idealHours = 8;
+    return idealHours * 7;
+}
+  
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours();
+  
+  if (actualSleepHours === idealSleepHours) {
+    console.log('Perfect Sleep Amount Achieved');
+  } else if (actualSleepHours > idealSleepHours)  {
+    console.log('You\'ve slept more than nessecsary');
+  } else if (actualSleepHours < idealSleepHours)  {
+    console.log('Go to bed, you need more rest');
+  }
